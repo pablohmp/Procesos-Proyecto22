455783427138fb8217fde5ec26b84ac4bc198384
PABLO HOWARD MASON PRECIADO <PabloHoward.Mason@alu.uclm.es>
2022-09-27 09:09:06 +0200

    Restructuracion de carpetas

--- cliente/spec/modeloSpec.js
+++ cliente/spec/modeloSpec.js
@@ -6,17 +6,28 @@ describe("El juego. . .", function() {
     miJuego = new Juego();
     miJuego.agregarUsuario("pepe");
     miJuego.agregarUsuario("luis");
-    usr1 = mijuego.usuarios['pepe'];
+    usr1 = miJuego.usuarios['pepe'];
     usr2 = miJuego.usuarios['luis'];
+  });
+
+  xit("inicialmente", function(){
+    let lista=miJuego.obtenerPartidas();
+    expected(lista.length).toEqual(0);
+    expect(usr1.nick).toEqual("pepe");
+    expect(usr2.nick).toEqual("luis");
   });
 
   it("crear partida", function(){
     let codigo = usr1.crearPartida();
     expect(miJuego.partidas[codigo]).toBeDefined();
     let partida = miJuego.partidas[codigo];
-    expect(partida.owner).toEqual(usr1.nick);
-    expect(partida.jugadores[0]).toEqual(usr1.nick);
+    expect(partida.owner.nick).toEqual(usr1.nick);
+    expect(partida.jugadores[0].nick).toEqual(usr1.nick);
     expect(partida.codigo).toEqual(codigo);
   })
+
+  xit("el usuario luis se une a la partida", function(){
+
+  });
 
 });
