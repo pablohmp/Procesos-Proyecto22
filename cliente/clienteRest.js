85fa0b5d16e32168c032dbd5fe1c3bd96e60dc2f
PabloHowardMason <PabloHoward.Mason@alu.uclm.es>
2022-10-19 09:35:48 +0200

    Updating

--- cliente/clienteRest.js
+++ cliente/clienteRest.js
@@ -1,58 +1,76 @@
-function ClienteRest() {
-    this.agregarUsuario = function (nick) {
-        var cli = this;
-        $.getJSON("/agregarUsuario/" + nick, function (data) {
-            //se ejecuta cuando conteste el servidor
-            console.log(data);
-            if (data.nick != -1) {
-                console.log("Usuario " + data.nick + " registrado")
-                //ws.nick=data.nick;
-                //$.cookie("nick",ws.nick);
-                //iu.mostrarHome(data);
-            }
-            else {
-                console.log("No se ha podido registrar el usuario")
-                //iu.mostrarModal("El nick ya está en uso");
-                //iu.mostrarAgregarJugador();
-            }
-        });
-        //todavia no estoy seguro que haya contestado el servidor
-        //lo que pongas aqui se ejecuta a la vez que la llamada
-    }
-    this.crearPartida = function (nick) {
-        var cli = this;
-        $.getJSON("/crearPartida/" + nick, function (data) {
-            //se ejecuta cuando conteste el servidor
-            console.log(data);
-            if (data.nick != -1) {
-                console.log("Usuario " + cli.nick + " crea una partida de codigo "+data.codigo)
-                //ws.nick=data.nick;
-                //$.cookie("nick",ws.nick);
-                //iu.mostrarHome(data);
-            }
-            else {
-                console.log("No se ha podido registrar el usuario")
-                //iu.mostrarModal("El nick ya está en uso");
-                //iu.mostrarAgregarJugador();
-            }
-        });
-    }
-    this.unirseAPartida = function (nick, codigo) {
-        var cli = this;
-        $.getJSON("/unirseAPartida/" + nick + "/" + codigo, function (data) {
-            //se ejecuta cuando conteste el servidor
-            console.log(data);
-            if (data.nick != -1) {
-                console.log("Usuario " + data.nick + " se une a partida de codigo "+data.codigo)
-                //ws.nick=data.nick;
-                //$.cookie("nick",ws.nick);
-                //iu.mostrarHome(data);
-            }
-            else {
-                console.log("No se ha podido registrar el usuario")
-                //iu.mostrarModal("El nick ya está en uso");
-                //iu.mostrarAgregarJugador();
-            }
-        });
-    }
+function ClienteRest(){
+	this.nick;	
+	this.agregarUsuario=function(nick){
+		let cli=this;
+		$.getJSON("/agregarUsuario/"+nick,function(data){
+			//se ejecuta cuando conteste el servidor
+			console.log(data);
+			if (data.nick!=-1){
+				console.log("Usuario "+data.nick+" registrado")
+				cli.nick=data.nick;
+				//ws.nick=data.nick;
+				$.cookie("nick",data.nick);
+				iu.mostrarHome();//iu.mostrarHome(data.nick)
+			}
+			else{
+				console.log("No se ha podido registrar el usuario")
+				iu.mostrarModal("El nick ya está en uso");
+				iu.mostrarAgregarUsuario();
+			}
+		});
+	}
+	this.crearPartida=function(){
+		let cli=this;
+		let nick=cli.nick;
+		$.getJSON("/crearPartida/"+nick,function(data){
+			console.log(data);
+			if (data.codigo!=-1){
+				console.log("Usuario "+nick+" crea partida codigo: "+data.codigo)
+				iu.mostrarCodigo(data.codigo);
+				//ws.nick=data.nick;
+				//$.cookie("nick",ws.nick);
+				//iu.mostrarHome(data);
+			}
+			else{
+				console.log("No se ha podido crear partida")
+				//iu.mostrarModal("El nick ya está en uso");
+				//iu.mostrarAgregarJugador();
+			}
+		});
+	}
+	this.unirseAPartida=function(codigo){
+		let cli=this;
+		$.getJSON("/unirseAPartida/"+cli.nick+"/"+codigo,function(data){
+			//se ejecuta cuando conteste el servidor
+			//console.log(data);
+			if (data.codigo!=-1){
+				console.log("Usuario "+cli.nick+" se une a partida codigo: "+data.codigo);
+				iu.mostrarCodigo(data.codigo);
+				//ws.nick=data.nick;
+				//$.cookie("nick",ws.nick);
+				//iu.mostrarHome(data);
+			}
+			else{
+				console.log("No se ha podido unir a partida")
+				//iu.mostrarModal("El nick ya está en uso");
+				//iu.mostrarAgregarJugador();
+			}
+		});
+	}
+	this.obtenerListaPartidas=function(){
+		let cli=this;
+		//obtenerPartidasDisponibles
+		$.getJSON("/obtenerPartidas",function(lista){
+			console.log(lista);
+			iu.mostrarListaDePartidas(lista);
+		});
+	}
+	this.obtenerListaPartidasDisponibles=function(){
+		let cli=this;
+		$.getJSON("/obtenerPartidasDisponibles",function(lista){
+			console.log(lista);
+			iu.mostrarListaDePartidasDisponibles(lista);
+		});
+	}
 }
+
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+const fs=require("fs");
 const express = require('express');
 const app = express();
 const modelo = require("./servidor/modelo.js");
@@ -7,54 +7,44 @@ const PORT = process.env.PORT || 3000;
 
 let juego = new modelo.Juego();
 
-// app.get('/', (req, res) => {
-//   res
-//     .status(200)
-//     .send("Hola")
-//     .end();
-// });
-
 app.use(express.static(__dirname + "/"));
 
-app.get("/", function (request, response) {
-    var contenido = fs.readFileSync(__dirname + "/cliente/index.html");
-    response.setHeader("Content-type", "text/html");
-    response.send(contenido);
+app.get("/", function(request,response){
+  var contenido=fs.readFileSync(__dirname+"/cliente/index.html");
+  response.setHeader("Content-type","text/html");
+  response.send(contenido);
 });
 
-app.get("/agregarUsuario/:nick", function (request, response) {
-    let nick = request.params.nick;
-    let res = juego.agregarUsuario(nick);
-    response.send(res);
+app.get("/agregarUsuario/:nick",function(request,response){
+  let nick = request.params.nick;
+  let res=juego.agregarUsuario(nick);
+  response.send(res); 
 });
 
-app.get("/crearPartida/:nick", function (request, response) {
-    let nick = request.params.nick;
-    let res = juego.jugadorCreaPartida(nick);
-    response.send(res);
+app.get("/crearPartida/:nick",function(request,response){
+  let nick = request.params.nick;
+  let res = juego.jugadorCreaPartida(nick);
+  response.send(res);
 });
 
-app.get("/unirseAPartida/:nick/:codigo", function (request, response) {
-    let nick = request.params.nick;
-    let codigo = request.params.codigo;
-    let res = juego.jugadorSeUneAPartida(nick, codigo);
-    response.send(res);
+app.get("/unirseAPartida/:nick/:codigo",function(request,response){
+  let nick = request.params.nick;
+  let codigo = request.params.codigo;
+  let res = juego.jugadorSeUneAPartida(nick,codigo);
+  response.send(res);
 });
 
-app.get("/obtenerPartidas", function (request, response) {
-    let res = juego.obtenerPartidas();
-    response.send(res);
+app.get("/obtenerPartidas",function(request,response){
+  let lista=juego.obtenerPartidas();
+  response.send(lista);
 });
 
-app.get("/obtenerPartidasDisponibles", function (request, response) {
-    let res = juego.obtenerPartidasDisponibles();
-    response.send(res);
+app.get("/obtenerPartidasDisponibles",function(request,response){
+  let lista=juego.obtenerPartidasDisponibles();
+  response.send(lista);
 });
-
-
-// Start the server
 
 app.listen(PORT, () => {
-    console.log(`App está escuchando en el puerto ${PORT}`);
-    console.log('Ctrl+C para salir');
+  console.log(`App está escuchando en el puerto ${PORT}`);
+  console.log('Ctrl+C para salir');
 });
