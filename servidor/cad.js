1c26096578f553d961f3f24cfc3d09a532600cca
PabloHowardMason <mrmason565@gmail.com>
2022-12-08 20:49:32 +0100

    añadido cad

---
+++ servidor/cad.js
@@ -0,0 +1,62 @@
+var mongo = require("mongodb").MongoClient;
+var ObjectID = require("mongodb").ObjectID;
+
+function Cad() {
+    this.logs;
+
+    //logs
+    this.insertarLog = function (log, callback) {
+        insertar(this.logs, partida, callback);
+    }
+
+    this.obtenerLogs = function (callback) {
+        obtenerTodos(this.logs, callback);
+    }
+
+    //Partidas
+    //Usuarios
+
+    //Productos
+
+    function insertar(coleccion, elemento, callback) {
+        coleccion.insertOne(elemento, function (err, result) {
+            if (err) {
+                console.log("error");
+            }
+            else {
+                console.log("Nuevo elemento creado");
+                callback(elemento);
+            }
+        });
+    };
+
+    function obtenerTodos(coleccion, callback) {
+        coleccion.find().toArray(function (error, col) {
+            callback(col);
+        });
+    };
+
+    this.conectar = function () {
+        let cad = this;
+        mongo.connect("mongodb+srv://batalla:batalla@cluster0.ngj1cva.mongodb.net/?retryWrites=true&w=majority", { useUnifiedTopology: true }, function (err, database) {
+            if (!err) {
+                console.log("Conectado a MongoDB Atlas");
+                database.db("batalla").collection("logs", function (err, col) {
+                    if (err) {
+                        console.log("No se puede obtener la coleccion")
+                    }
+                    else {
+                        console.log("tenemos la colección logs");
+                        cad.logs = col;
+                    }
+                });
+
+            } else {
+                console.log("No se pudo conectar con MongoDB Atlas");
+            }
+        });
+    }
+    //this.conectar();
+}
+
+module.exports.Cad = Cad;
