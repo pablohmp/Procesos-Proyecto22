726451be937e319bd416ec4baa4aaba3a57e8d2b
PabloHowardMason <mrmason565@gmail.com>
2023-01-11 17:18:07 +0100

    cambio visual al inicio de sesion con Google

--- cliente/controlWeb.js
+++ cliente/controlWeb.js
@@ -13,7 +13,10 @@ function ControlWeb() {
         //cadena = cadena + '<button id="btnAU" class="btn btn-primary mb-2 mr-sm-2">Entrar</button>';
         
         cadena = cadena + '<button id="btnAU" class="btn btn-primary mb-2 mr-sm-2">Iniciar Sesión</button>';
-        cadena = cadena + '<a href="/auth/google" class="btn btn-primary mb-2 mr-sm-2">Accede con Google</a>';
+        cadena = cadena + '<br><br>';
+        cadena = cadena + '<p>O inicia sesión con Google:</p>';
+        cadena = cadena + '<a href="/auth/google"><img src="cliente/img/icons8-logo-de-google-96.svg" alt="" width="50" height="50"></a>';
+        //cadena = cadena + '<a href="/auth/twitter"><img src="cliente/img/icons8-twitter-96.svg" alt="" width="50" height="50"></a>';
         
         cadena = cadena + '<div id="nota"></div>';
         cadena = cadena + '</div></div>'
